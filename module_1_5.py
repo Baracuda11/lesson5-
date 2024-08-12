5c8361f0a7bbaa4bc1a80e5dd49b7874e64c077e
pipin <Aleksejsoronov3@gmail.com>
2024-08-12 20:41:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+imutable_var = ("green", 'red', "bleck", 1, 2, 3,)
+print(imutable_var)
+# imutable_var.append (["green", 2])
+# # imutable_var[0]="gold" нельзя изменить значение элементов так как смешанный тип данных в переменной
+mutable_list = ["Egor,", "Denis", "Kirill"]
+print(mutable_list)
+mutable_list[0] = "Jamsut"
+print(mutable_list)
+mutable_list[1] = 'Anna'
+print(mutable_list)
+mutable_list[2] = 'Alina'
+print(mutable_list)
